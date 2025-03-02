990403853a240a1a219ecf99c56710b8b4115e11
DHARANISHKBIT <dharanish.it23@bitsathy.ac.in>
2025-03-02 11:42:14 +0530

    commit message

--- src/Pages/Signpage/Signpage.jsx
+++ src/Pages/Signpage/Signpage.jsx
@@ -67,7 +67,7 @@ const SingFrom = () => {
           </div>
           <div onClick={handleSubmit}>
             <ButtonComponent
-            buttonText="Sing-up now"
+            buttonText="Sing-up"
             bgColor="bg-blue-500"
             />
           </div>
