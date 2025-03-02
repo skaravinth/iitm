146f831fb979a3498025e3abff6a6f256256b119
DHARANISHKBIT <dharanish.it23@bitsathy.ac.in>
2025-03-02 13:23:13 +0530

    create header

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Profile from "../../assets/profile.jpg"
+import Profilepage from "../Profilepage/Profile";
+import Summa from "../Dialog/summa";
+
+const Header = () => {
+  const [Popup,setPopup] = useState(false)
+  return (
+    <div className="text-black  px-6 py-3 flex justify-between items-center border border-gray-300  shadow-lg">
+      <div className="text-xl font-bold">Dogify</div>
+{Popup && (<div>
+        <Summa children={<Profilepage setPopup={setPopup} />} direction="right" borderRadius="12px" />
+      </div>)}
+      <div className="flex justify-end items-center">
+      <div className="text-black font-bold mr-15">ABOUT</div>
+    <div className="text-black font-bold mr-15">SHOPPING PAGE</div>
+      <div className="p-2  rounded-full" onClick={()=> setPopup(!Popup)}>     
+  <img src={Profile} className="w-12 h-12 rounded-full" />
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
